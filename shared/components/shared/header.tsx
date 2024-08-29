f3d699ff7c93733aa790e1d494e8c3f10d3dcd28
berdibek-2002 <uralov0212@gmail.com>
2024-08-29 11:45:41 +0500

    version 1.0.2

--- shared/components/shared/header.tsx
+++ shared/components/shared/header.tsx
@@ -47,26 +47,26 @@ export const Header: React.FC<Props> = ({ hasSearch = true, hasCart = true, clas
 
   return (
     <header className={cn('border-b', className)}>
-      <Container className="flex items-center justify-between py-8">
+      <Container className='flex items-center justify-between py-8'>
         {/* Левая часть */}
-        <Link href="/">
-          <div className="flex items-center gap-4">
-            <Image src="/logo.png" alt="Logo" width={35} height={35} />
+        <Link href='/'>
+          <div className='flex items-center gap-4'>
+            <Image src='/logo.png' alt='Logo' width={35} height={35} />
             <div>
-              <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
-              <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+              <h1 className='text-2xl uppercase font-black'>Friday Pizza</h1>
+              <p className='text-sm text-gray-400 leading-3'>вкусней уже некуда</p>
             </div>
           </div>
         </Link>
 
         {hasSearch && (
-          <div className="mx-10 flex-1">
+          <div className='mx-10 flex-1'>
             <SearchInput />
           </div>
         )}
 
         {/* Правая часть */}
-        <div className="flex items-center gap-3">
+        <div className='flex items-center gap-3'>
           <AuthModal open={openAuthModal} onClose={() => setOpenAuthModal(false)} />
 
           <ProfileButton onClickSignIn={() => setOpenAuthModal(true)} />
